99b7cbcb50510cfdf58012f811e0de0b5555588b
dylanljones <dylanljones94@gmail.com>
2022-04-24 00:03:08 +0200

    docs: use furo sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 import math
+from pkg_resources import get_distribution
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -24,7 +25,10 @@ import pyrekordbox
 project = "pyrekordbox"
 copyright = "2022, Dylan Jones"
 author = "Dylan L. Jones"
-
+release = pyrekordbox.__version__
+version = release
+for sp in "abcfr":
+    version = version.split(sp)[0]
 
 # -- General configuration ---------------------------------------------------
 
@@ -36,6 +40,7 @@ extensions = [
     "numpydoc",
     "myst_parser",
     "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
     "matplotlib.sphinxext.plot_directive",
     "sphinx.ext.intersphinx",  # links to numpy, scipy ... docs
     "sphinx.ext.viewcode",
@@ -63,12 +68,15 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "tests"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"  # "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Code block styling
+pygments_dark_style = "monokai"
 
 # Include Markdown parser
 # source_parsers = {
