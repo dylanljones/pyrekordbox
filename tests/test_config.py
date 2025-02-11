0c1f6a585fd7c80c03a11fa6dd8b0ffa8f5f49f2
dylanljones <dylanljones94@gmail.com>
2025-02-11 22:07:00 +0100

    fix config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,7 @@
 # Date:   2023-10-02
 
 import json
+import sys
 from pathlib import Path
 
 import pytest
@@ -31,9 +32,9 @@ def mock_rekordbox_options_file(agent_storage_dir, db_path):
         json.dump(options, fp)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def pioneer_app_dir(tmp_path_factory):
-    root = tmp_path_factory.mktemp("Pioneer")
+    root = tmp_path_factory.mktemp("app")
     rb_dir = root / "rekordbox"
     rb6_dir = root / "rekordbox6"
     agent_storage_dir = root / "rekordboxAgent" / "storage"
@@ -57,18 +58,28 @@ def pioneer_app_dir(tmp_path_factory):
     return root
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def pioneer_install_dir(tmp_path_factory):
-    root = tmp_path_factory.mktemp("Pioneer")
-    rb5_dir = root / "rekordbox 5.1.1"
-    rb6_dir = root / "rekordbox 6.1.1"
-    rb5_dir_alt = root / "rekordbox 5.1.2"
-    rb6_dir_alt = root / "rekordbox 6.1.2"
+    root = tmp_path_factory.mktemp("install")
+    if sys.platform == "win32":
+        root_56 = root / "Pioneer"
+        root_7 = root / "rekordbox"
+    else:
+        root_56 = root
+        root_7 = root
+    rb5_dir = root_56 / "rekordbox 5.1.1"
+    rb6_dir = root_56 / "rekordbox 6.1.1"
+    rb7_dir = root_7 / "rekordbox 7.1.1"
+    rb5_dir_alt = root_56 / "rekordbox 5.1.2"
+    rb6_dir_alt = root_56 / "rekordbox 6.1.2"
+    rb7_dir_alt = root_7 / "rekordbox 7.1.2"
 
     rb5_dir.mkdir(parents=True)
     rb6_dir.mkdir(parents=True)
+    rb7_dir.mkdir(parents=True)
     rb5_dir_alt.mkdir(parents=True)
     rb6_dir_alt.mkdir(parents=True)
+    rb7_dir_alt.mkdir(parents=True)
     return root
 
 
@@ -86,13 +97,14 @@ def test_pioneer_config(pioneer_install_dir, pioneer_app_dir):
 def test_rb5_config(pioneer_install_dir, pioneer_app_dir):
     update_config(pioneer_install_dir, pioneer_app_dir)
     expected_version = "5.1.2"
+    if sys.platform == "win32":
+        pioneer_install_dir = pioneer_install_dir / "Pioneer"
 
     app_dir = get_config("rekordbox5", "app_dir")
     install_dir = get_config("rekordbox5", "install_dir")
     db_dir = get_config("rekordbox5", "db_dir")
     db_path = get_config("rekordbox5", "db_path")
     version = get_config("rekordbox5", "version")
-
     assert isinstance(install_dir, Path)
     assert isinstance(app_dir, Path)
     assert isinstance(db_dir, Path)
@@ -107,6 +119,8 @@ def test_rb5_config(pioneer_install_dir, pioneer_app_dir):
 def test_rb6_config(pioneer_install_dir, pioneer_app_dir):
     update_config(pioneer_install_dir, pioneer_app_dir)
     expected_version = "6.1.2"
+    if sys.platform == "win32":
+        pioneer_install_dir = pioneer_install_dir / "Pioneer"
 
     app_dir = get_config("rekordbox6", "app_dir")
     install_dir = get_config("rekordbox6", "install_dir")
@@ -125,15 +139,42 @@ def test_rb6_config(pioneer_install_dir, pioneer_app_dir):
     assert version == expected_version
 
 
+def test_rb7_config(pioneer_install_dir, pioneer_app_dir):
+    update_config(pioneer_install_dir, pioneer_app_dir)
+    expected_version = "7.1.2"
+    if sys.platform == "win32":
+        pioneer_install_dir = pioneer_install_dir / "rekordbox"
+
+    app_dir = get_config("rekordbox7", "app_dir")
+    install_dir = get_config("rekordbox7", "install_dir")
+    db_dir = get_config("rekordbox7", "db_dir")
+    db_path = get_config("rekordbox7", "db_path")
+    version = get_config("rekordbox7", "version")
+
+    assert isinstance(install_dir, Path)
+    assert isinstance(app_dir, Path)
+    assert isinstance(db_dir, Path)
+    assert isinstance(db_path, Path)
+    assert install_dir == (pioneer_install_dir / f"rekordbox {expected_version}")
+    assert app_dir == (pioneer_app_dir / "rekordbox6")
+    assert db_dir == (pioneer_app_dir / "rekordbox")
+    assert db_path == (pioneer_app_dir / "rekordbox" / "master.db")
+    assert version == expected_version
+
+
 def test_rb5_config_alt(pioneer_install_dir, pioneer_app_dir):
     # test default: latest version
     update_config(pioneer_install_dir, pioneer_app_dir)
     expected_version = "5.1.2"
+    if sys.platform == "win32":
+        full_pioneer_install_dir = pioneer_install_dir / "Pioneer"
+    else:
+        full_pioneer_install_dir = pioneer_install_dir
 
     app_dir = get_config("rekordbox5", "app_dir")
     install_dir = get_config("rekordbox5", "install_dir")
     version = get_config("rekordbox5", "version")
-    assert install_dir == (pioneer_install_dir / f"rekordbox {expected_version}")
+    assert install_dir == (full_pioneer_install_dir / f"rekordbox {expected_version}")
     assert app_dir == (pioneer_app_dir / "rekordbox")
     assert version == expected_version
 
@@ -146,7 +187,7 @@ def test_rb5_config_alt(pioneer_install_dir, pioneer_app_dir):
     app_dir = get_config("rekordbox5", "app_dir")
     install_dir = get_config("rekordbox5", "install_dir")
     version = get_config("rekordbox5", "version")
-    assert install_dir == (pioneer_install_dir / f"rekordbox {expected_version}")
+    assert install_dir == (full_pioneer_install_dir / f"rekordbox {expected_version}")
     assert app_dir == (pioneer_app_dir / "rekordbox")
     assert version == expected_version
 
@@ -155,11 +196,15 @@ def test_rb6_config_alt(pioneer_install_dir, pioneer_app_dir):
     # test default: latest version
     update_config(pioneer_install_dir, pioneer_app_dir)
     expected_version = "6.1.2"
+    if sys.platform == "win32":
+        full_pioneer_install_dir = pioneer_install_dir / "Pioneer"
+    else:
+        full_pioneer_install_dir = pioneer_install_dir
 
     app_dir = get_config("rekordbox6", "app_dir")
     install_dir = get_config("rekordbox6", "install_dir")
     version = get_config("rekordbox6", "version")
-    assert install_dir == (pioneer_install_dir / f"rekordbox {expected_version}")
+    assert install_dir == (full_pioneer_install_dir / f"rekordbox {expected_version}")
     assert app_dir == (pioneer_app_dir / "rekordbox6")
     assert version == expected_version
 
@@ -172,6 +217,36 @@ def test_rb6_config_alt(pioneer_install_dir, pioneer_app_dir):
     app_dir = get_config("rekordbox6", "app_dir")
     install_dir = get_config("rekordbox6", "install_dir")
     version = get_config("rekordbox6", "version")
-    assert install_dir == (pioneer_install_dir / f"rekordbox {expected_version}")
+    assert install_dir == (full_pioneer_install_dir / f"rekordbox {expected_version}")
+    assert app_dir == (pioneer_app_dir / "rekordbox6")
+    assert version == expected_version
+
+
+def test_rb7_config_alt(pioneer_install_dir, pioneer_app_dir):
+    # test default: latest version
+    update_config(pioneer_install_dir, pioneer_app_dir)
+    expected_version = "7.1.2"
+    if sys.platform == "win32":
+        full_pioneer_install_dir = pioneer_install_dir / "rekordbox"
+    else:
+        full_pioneer_install_dir = pioneer_install_dir
+
+    app_dir = get_config("rekordbox7", "app_dir")
+    install_dir = get_config("rekordbox7", "install_dir")
+    version = get_config("rekordbox7", "version")
+    assert install_dir == (full_pioneer_install_dir / f"rekordbox {expected_version}")
+    assert app_dir == (pioneer_app_dir / "rekordbox6")
+    assert version == expected_version
+
+    # test alternative version
+    update_config(
+        pioneer_install_dir, pioneer_app_dir, rb7_install_dirname="rekordbox 7.1.1"
+    )
+    expected_version = "7.1.1"
+
+    app_dir = get_config("rekordbox7", "app_dir")
+    install_dir = get_config("rekordbox7", "install_dir")
+    version = get_config("rekordbox7", "version")
+    assert install_dir == (full_pioneer_install_dir / f"rekordbox {expected_version}")
     assert app_dir == (pioneer_app_dir / "rekordbox6")
     assert version == expected_version
