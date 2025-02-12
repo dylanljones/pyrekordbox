94969f1d49cb2ec7c76b41e9fc70c2796bca4c1b
dylanljones <dylanljones94@gmail.com>
2025-02-12 16:56:29 +0100

    fix(db): remove deprecated `xml` module
    BREAKING CHANGE: remove `xml` module
    
    Use the `rbxml` module instead!

--- pyrekordbox/xml.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Dylan Jones
-# Date:   2022-04-10
-
-from .rbxml import *  # noqa: F401, F403
-from .utils import warn_deprecated as _warn_deprecated
-
-_warn_deprecated("pyrekordbox.xml", "pyrekordbox.rbxml", remove_in="0.4.0")
